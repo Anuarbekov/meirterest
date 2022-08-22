ded7f4cb43de5fe82cb9e7536e4c57a1d20f37fd
Meir <mikonek.a@gmail.com>
2022-08-22 14:48:32 +0600

    header & css

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,28 @@
 import axios from "axios";
 import { useState } from "react";
 import TextField from "@mui/material/TextField";
-import CssBaseline from "@mui/material/CssBaseline";
-import Masonry from "react-responsive-masonry";
-import { AppBar, Toolbar, Typography, makeStyles } from "@material-ui/core";
-const useStyles = makeStyles((theme) => ({
-  navlinks: {},
-  logo: {
-    flexGrow: "1",
-    cursor: "pointer",
-  },
-  link: {
-    textDecoration: "none",
-    color: "white",
-    fontSize: "20px",
-    marginLeft: theme.spacing(20),
-    cursor: "pointer",
-  },
-}));
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import Head from "next/head";
+import Avatar from "@mui/material/Avatar";
 export default function Home() {
-  const classes = useStyles();
   const [images, setImages] = useState([]);
   const [queryString, setQueryString] = useState("");
   const [page, setPage] = useState(1);
   const getImages = async (query, page) => {
     const user_key = "40kJkObwZKgcDycHW6Z0UDzoRJF8JIAIiW_CjkbjZEY";
     const result = await axios.get(
-      `https://api.unsplash.com/search/photos?query=${query}&page=${page}&per_page=30&client_id=${user_key}`
+      `https://api.unsplash.com/search/photos?query=${query}&page=${page}&per_page=10&client_id=${user_key}`
     );
     setImages(result.data.results);
   };
   return (
     <div>
-      <AppBar position="sticky">
-        <CssBaseline />
-        <Toolbar>
-          <Typography variant="h5" className={classes.logo}>
-            Navbar
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      <Head>
+        <title>Meirterest</title>
+      </Head>
+      <header className="header">
+        <h5>Meirterest</h5>
+      </header>
       <div className="input">
         <TextField
           id="outlined-search"
@@ -50,15 +33,38 @@ export default function Home() {
         <button onClick={() => getImages(queryString, page)}>Search</button>
       </div>
       <div className="images">
-        <Masonry columnsCount={4} gutter="5px">
-          {images.map((image) => (
-            <img
-              key={image.id}
-              src={image.urls.regular}
-              style={{ width: "100%", display: "block", borderRadius: 10 }}
-            />
-          ))}
-        </Masonry>
+        <ResponsiveMasonry columnsCountBreakPoints={{ 750: 2, 900: 3 }}>
+          <Masonry gutter="0.5vw">
+            {images.map((image) => (
+              <div key={image.id}>
+                <img
+                  key={image.id}
+                  src={image.urls.full}
+                  style={{
+                    width: "100%",
+                    display: "block",
+                    borderRadius: 10,
+                  }}
+                />
+                <div className="author-description">
+                  {image.user.profile_image ? (
+                    <Avatar
+                      sx={{ height: "5vh", width: "5vh" }}
+                      src={image.user.profile_image.large}
+                    />
+                  ) : (
+                    <Avatar src="/broken-image.jpg" />
+                  )}
+                  {image.user.username ? (
+                    <h5 className="image-description">{image.user.username}</h5>
+                  ) : (
+                    <h5 className="image-description">Anonymous</h5>
+                  )}
+                </div>
+              </div>
+            ))}
+          </Masonry>
+        </ResponsiveMasonry>
       </div>
     </div>
   );
